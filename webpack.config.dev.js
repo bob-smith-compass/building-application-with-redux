9a55cfa95385fa7899b01a70585784230559ab40
David <david.shams@gmail.com>
2019-05-29 21:50:53 -0500

    webpack overlay and CORS

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 process.env.NODE_ENV = 'development';
 
 module.exports = {
-    
+    mode: 'development',
+    target: 'web',
+    devTool: 'cheap-module-source-map',
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: '/',
+        filename: 'bundle.js'
+
+    },
+    devServer: {
+        stats: 'minimal',
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: {"Access-Control-Allow-Origin": "*"},
+        https: false
+    }
 }
 
