6608b0e1b78cdec9bafcbccde62ebf96864f9b61
David <david.shams@gmail.com>
2019-05-29 21:44:33 -0500

    webpack initial

---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+    
+}
+
