616b79c8ebe03cac5cd81668261aca5ac283d687
David <david.shams@gmail.com>
2019-05-30 21:38:09 -0500

    don't mix div flex and ul flex

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom'
 import './main.css'
 
 function Hi() {
-    return (<div id="container">
-        <ul id="flex-container">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-        </ul>
-    <div className="flex-item">1</div>
+    return (
+        // <div id="container">
+        <div>
+            <ul id="flex-container">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+            </ul>
+            {/* <div className="flex-item">1</div>
     <div className="flex-item">2</div>
     <div className="flex-item">3</div>
     <div className="flex-item">4</div>
-    <div className="flex-item">5</div>
-        </div>)
+    <div className="flex-item">5</div> */}
+        </div>
+    )
 }
 
 ReactDOM.render(<Hi />, document.getElementById('app'))
