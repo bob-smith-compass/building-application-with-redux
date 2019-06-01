80ffa90f83db9b36608d1586e3bd0f9f1ab5aa03
David <david.shams@gmail.com>
2019-06-01 17:42:04 -0500

    courseReducer with default you can import as any name

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-export default function courseReducer(state = [] /** initialize state to empty array */, action) {
+export default /** courseReducer with default you can import as any name */ function courseReducer(state = [] /** initialize state to empty array */, action) {
     switch (action.type) {
         case 'CREATE_COURSE':
             return [...state, {...action.course}]
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,8 @@
 import {combineReducers} from 'redux'
+import course /** courseReducer with default you can import as any name */  from './courseReducer'
+
+const rootRecucer = combineReducers({
+    course: course
+})
+
+export default rootRecucer;
