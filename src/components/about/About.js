3ab08dbe8d4d9198bec01e3d98dc51bb7a420a3f
David <david.shams@gmail.com>
2019-06-01 00:17:27 -0500

    flex style

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./about.css"
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className="about">
+            <ul>
+                <li>Main</li>
+                <li><div className="btn btn-primary">Button</div></li>
+                <li><div className="btn btn-primary">Button</div></li>
+            </ul>
         </div>
     )
 }
