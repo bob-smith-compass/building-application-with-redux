16e0c1dcad408f17341d0fbc4ddb90a090cc4d3c
David <david.shams@gmail.com>
2019-06-01 17:15:16 -0500

    event.preventDefault(); STOP from submitting form

--- src/components/coursepage/CoursePage.js
+++ src/components/coursepage/CoursePage.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react'
 
 export default class CoursePage extends Component {
     // constructor(props){
-        // super(props)
-        // this.state = {
-        state = {
-            course: {
-                title: ""
-            }
+    // super(props)
+    // this.state = {
+    state = {
+        course: {
+            title: ""
         }
-        // this.setState = this.setState.bind(this)
-        // this.handleChange = this.handleChange.bind(this)
+    }
+    // this.setState = this.setState.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
     // }
     handleChange = (event) => {
-    // handleChange(event) {
-        // event.preventDefalut()
-        const course = {...this.state.course, title: event.target.value};
-        this.setState({course})
+        // handleChange(event) {
+        const course = { ...this.state.course, title: event.target.value };
+        this.setState({ course })
+    }
+    handleSubmit = (event) => {
+        // console.log(event);
+        event.preventDefault();
+        console.log(`this.state.course.title`)
     }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 {JSON.stringify(this.state)}
                 <h2>Courses</h2>
                 <h3>Add Course</h3>
                 <input onChange={this.handleChange} value={this.state.course.title} />
-                <input type="submit" value="Save"/>
-                
+                <input type="submit" value="Save" />
+
             </form>
         )
     }
