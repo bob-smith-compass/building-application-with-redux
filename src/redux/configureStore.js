3c5f96bd86eb64a74e6125ab4ec0ddbea67488fa
David <david.shams@gmail.com>
2019-06-01 17:50:33 -0500

    return createStore(rootRecucer, initialState)

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,5 +2,5 @@ import { createStore } from 'redux'
 import rootRecucer from './reducers' // index.js implied
 
 export default configureStore(initialState) {
-    
+    return createStore(rootRecucer, initialState);
 }
