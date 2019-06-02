b03331c7e1e217f43eb98062fa15dbe5ff07b23b
David <david.shams@gmail.com>
2019-06-01 20:16:17 -0500

    works

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootRecucer from './reducers'; // index.js implied
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
 
-// export default function configureStore(initialState) {
-//     // const composeEnhancer = window.__REDUX_DEVTOOL_ESTENSION_COMPOSE__ || compose;
-//     return createStore(rootRecucer, initialState, applyMiddleware(reduxImmutableStateInvariant()));
-// }
-
 export default function configureStore(initialState) {
+    const composeEnhancer = window.__REDUX_DEVTOOL_ESTENSION_COMPOSE__ || compose;
     return createStore(rootRecucer, initialState, applyMiddleware(reduxImmutableStateInvariant()));
 }
